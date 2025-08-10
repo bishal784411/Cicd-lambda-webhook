3464a11b0b3597e4ab31c51253e4633702119ac3
bishal784411 <timsinabishal232@gmail.com>
2025-08-10 11:45:25 +0545

    Testing the webhook

--- try.py
+++ try.py
@@ -4,7 +4,6 @@ def lambda_handler(event, context):
     print("Received event:")
     print(json.dumps(event))  # logs full event payload
 
-    # You can inspect event['body'] for GitHub webhook payload (string)
     if 'body' in event:
         print("Webhook body:")
         print(event['body'])
