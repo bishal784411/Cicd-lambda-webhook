baf77161d047ad01a718648a9a883998a645ffca
bishal784411 <timsinabishal232@gmail.com>
2025-08-10 13:11:05 +0545

    Testi

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -12,5 +12,5 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps({"message": "This is"})
+        'body': json.dumps({"message": "This i   sjdfshdf sdhfishdflishadflis"})
     }
