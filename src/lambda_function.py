4170c3fee575fbb1ef691a73b5ff3743d204b07b
bishal784411 <timsinabishal232@gmail.com>
2025-08-10 12:11:30 +0545

    Testing

---
+++ src/lambda_function.py
@@ -0,0 +1,16 @@
+import json
+
+def lambda_handler(event, context):
+    print("Received event:")
+    print(json.dumps(event))
+
+    if 'body' in event:
+        payload = json.loads(event['body'])
+        commit_id = payload.get('head_commit', {}).get('id')
+        commit_msg = payload.get('head_commit', {}).get('message')
+        print(f"Commit ID: {commit_id}, Commit Message: {commit_msg}")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"message": "This is test"})
+    }
